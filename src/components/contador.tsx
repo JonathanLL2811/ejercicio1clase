1ea3364aa76af3ed922885e5ed2c4021d3ab8a66
JonathanLL2811 <156973028+JonathanLL2811@users.noreply.github.com>
2024-10-19 18:22:00 -0600

    tarea terminada
    ya por fin

---
+++ src/components/contador.tsx
@@ -0,0 +1,44 @@
+// src/components/Contador.tsx
+import React, { useState, useEffect } from 'react';
+
+const Contador: React.FC = () => {
+    const [texto, setTexto] = useState('');  
+    const [contador, setContador] = useState (0); 
+    const [colorTexto, setColorTexto] = useState('blue'); 
+
+    useEffect(() => {
+        setContador(texto.length); 
+
+        if (texto.length === 0) {
+            setColorTexto('blue'); 
+        } else if (texto.length < 10) {
+            setColorTexto('red');
+        } else if (texto.length >= 10 && texto.length <= 20) {
+            setColorTexto('orange');
+        } else {
+            setColorTexto('green');
+        }
+    }, [texto]);
+
+    const manejarCambioTexto = (e: any) => {
+        setTexto(e.target.value);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={texto}
+                onChange={manejarCambioTexto}
+                placeholder="Escribe algo..."
+            />
+            <p style={{ color: colorTexto }}>
+                Contador de texto ingresados: {contador}
+            </p>
+        </div>
+    );
+};
+
+export default Contador;
+
+
